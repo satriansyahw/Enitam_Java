7035f0f115e5ab23a580c6edf1dfea3951a936c1
Satriansyah <satriansyahw@gmail.com>
2022-09-06 00:41:09 -0600

    initial project

---
+++ enitam/src/main/java/com/arkir/enitam/EnitamApplication.java
@@ -0,0 +1,13 @@
+package com.arkir.enitam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnitamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnitamApplication.class, args);
+	}
+
+}
